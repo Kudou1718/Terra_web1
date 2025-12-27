ec78baa07405fcac2fea14c0c2010463423a6368
Kudou1718 <doob1212@naver.com>
2025-12-27 14:39:07 +0000

    log1

--- src/components/BattleSystem.jsx
+++ src/components/BattleSystem.jsx
@@ -2,16 +2,14 @@ import styles from './styles/BattleSystem.module.css'
 
 export default function BattleSystem({ player, enemy, battleLog }) {
   return (
-    <div style={{ display: "flex", flexDirection: "column", height: "100%" }}>
+    <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
       <h2>⚔️ 전투 개시!</h2>
       <p>플레이어 HP: {player.hp}</p>
       <p>{enemy.name} HP: {enemy.hp}</p>
 
-      <div className={styles.battleLog}>
-        {battleLog.map((log, i) => (
-          <p key={i}>{log}</p>
-        ))}
-      </div>
+      {battleLog.map((log, i) => (
+        <p key={i}>{log}</p>
+      ))}
     </div>
   )
 }
